c171364d0172e04abb6ea2b1ae739cad245dca5c
jurajjasik <juraj.jasik@gmail.com>
2023-05-31 12:05:00 +0200

    clean-up

--- src/CubicSplineInterp.cpp
+++ src/CubicSplineInterp.cpp
@@ -5,87 +5,87 @@
 
 void CubicSplineInterp::init(const float* x, const float* y, int n){
     while (_inCalc) {}
-    
+
     _notValid = true;
-    
-	_x = x;
-	_y = y;
-	_n = n > CSI_MAX_TAB_POINTS ? CSI_MAX_TAB_POINTS : n;
-	
-	float u[_n];
-	
-	_y2[0] = u[0] = 0.0; // natural spline
-	
-	for (int i = 1; i < _n-1; ++i) { 
-		float sig = ( x[i] - x[i-1] ) / ( x[i+1] - x[i-1] );
-		float p = sig * _y2[i-1] + 2.0;
-		_y2[i] = ( sig - 1.0 ) / p;
-		u[i] = ( y[i+1] - y[i] ) / ( x[i+1] - x[i] ) 
-			- ( y[i] - y[i-1] ) / ( x[i] - x[i-1] );
-		u[i] = ( 6.0 * u[i] / ( x[i+1] - x[i-1] ) - sig * u[i-1] ) / p;
-	}
-	
-	_y2[_n-1] = 0.0;
-	
-	for ( int i = _n-2; i > 0; --i) {
-		_y2[i] = _y2[i] * _y2[i+1] + u[i];
-	}
-    
+
+    _x = x;
+    _y = y;
+    _n = n > CSI_MAX_TAB_POINTS ? CSI_MAX_TAB_POINTS : n;
+
+    float u[_n];
+
+    _y2[0] = u[0] = 0.0; // natural spline
+
+    for (int i = 1; i < _n-1; ++i) {
+        float sig = ( x[i] - x[i-1] ) / ( x[i+1] - x[i-1] );
+        float p = sig * _y2[i-1] + 2.0;
+        _y2[i] = ( sig - 1.0 ) / p;
+        u[i] = ( y[i+1] - y[i] ) / ( x[i+1] - x[i] )
+            - ( y[i] - y[i-1] ) / ( x[i] - x[i-1] );
+        u[i] = ( 6.0 * u[i] / ( x[i+1] - x[i-1] ) - sig * u[i-1] ) / p;
+    }
+
+    _y2[_n-1] = 0.0;
+
+    for ( int i = _n-2; i > 0; --i) {
+        _y2[i] = _y2[i] * _y2[i+1] + u[i];
+    }
+
     _klo = 0;
-    
+
     _notValid = false;
 }
 
 float CubicSplineInterp::calc(float x) const
 {
     if (_notValid) return 0.0;
-    
-	int klo = 0; 
-	int khi = _n - 1;
-	while ( khi - klo > 1 ) {
-		int k = ( khi + klo ) >> 1;
-		if ( _x[k] > x ) khi = k;
-		else klo = k;
-	} 
-	
-	float h = _x[khi] - _x[klo];
-	if ( !( h > 0.0 || h < 0.0 ) )
-		return 0.0;
-	float a = ( _x[khi] - x ) / h;
-	float b = ( x - _x[klo] ) / h; 
-	return a * _y[klo] + b * _y[khi] + 
-		( ( a * a * a - a ) * _y2[klo] + ( b * b * b - b ) * _y2[khi] ) * ( h * h ) / 6.0;
+
+    int klo = 0;
+    int khi = _n - 1;
+    while ( khi - klo > 1 ) {
+        int k = ( khi + klo ) >> 1;
+        if ( _x[k] > x ) khi = k;
+        else klo = k;
+    }
+
+    float h = _x[khi] - _x[klo];
+    if ( !( h > 0.0 || h < 0.0 ) )
+        return 0.0;
+    float a = ( _x[khi] - x ) / h;
+    float b = ( x - _x[klo] ) / h;
+    return a * _y[klo] + b * _y[khi] +
+        ( ( a * a * a - a ) * _y2[klo] + ( b * b * b - b ) * _y2[khi] ) * ( h * h ) / 6.0;
 }
 
 float CubicSplineInterp::calcHunt(float x)
 {
     if (_notValid) return 0.0;
-    
-	_hunt(x, &_klo);
-    
+
+    _hunt(x, &_klo);
+
     int klo = _klo;
-    
+
     if(klo >= _n - 1) klo = _n - 2;
     if(klo < 0 ) klo = 0;
-    
+
     CSI_TRACE( printf("calcHunt => klo: %d\r\n", klo ); )
-	
-	float h = _x[klo + 1] - _x[klo];
-    
-	if ( !( h > 0.0 || h < 0.0 ) )
-		return 0.0;
-	float a = ( _x[klo + 1] - x ) / h;
-	float b = ( x - _x[klo] ) / h; 
-	return a * _y[klo] + b * _y[klo + 1] + 
-		( ( a * a * a - a ) * _y2[klo] + ( b * b * b - b ) * _y2[klo + 1] ) * ( h * h ) / 6.0;
+
+    float h = _x[klo + 1] - _x[klo];
+
+    if ( !( h > 0.0 || h < 0.0 ) )
+        return 0.0;
+    float a = ( _x[klo + 1] - x ) / h;
+    float b = ( x - _x[klo] ) / h;
+    return a * _y[klo] + b * _y[klo + 1] +
+        ( ( a * a * a - a ) * _y2[klo] + ( b * b * b - b ) * _y2[klo + 1] ) * ( h * h ) / 6.0;
 }
 
 void CubicSplineInterp::_hunt(float x, int *jlo){
     _inCalc = true;
-    
+
     int jhi;
     int ascnd = ( _x[_n - 1] >= _x[0] ); //True if ascending order of table, false otherwise.
-    
+
     if(ascnd){
         if (x > _x[_n - 1]) {
             *jlo = _n;
@@ -105,15 +105,15 @@ void CubicSplineInterp::_hunt(float x, int *jlo){
             return;
         }
     }
-    
+
     if (*jlo < 0 || *jlo >= _n) { //Input guess not useful. Go immediately to bisection
-        *jlo = -1; 
+        *jlo = -1;
         jhi = _n - 1;
     } else {
         unsigned int inc = 1; // Set the hunting increment.
         if ( x >= _x[*jlo] == ascnd ) { // Hunt up:
             CSI_TRACE( printf("_hunt => hunt up\r\n"); )
-            if (*jlo == _n - 1) 
+            if (*jlo == _n - 1)
                 return;
             jhi = (*jlo) + 1;
             while ( x >= _x[jhi] == ascnd ) { // Not done hunting,
@@ -150,7 +150,7 @@ void CubicSplineInterp::_hunt(float x, int *jlo){
         else
             jhi = jm;
     }
-    
+
     _inCalc = false;
 }
 
--- src/CubicSplineInterp.h
+++ src/CubicSplineInterp.h
@@ -5,7 +5,7 @@
 
 #define CSI_MAX_TAB_POINTS 64
 
-// Documentation: Numerical Recipes in C - The Art of Scientific Computing, 
+// Documentation: Numerical Recipes in C - The Art of Scientific Computing,
 // W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery, CAMBRIDGE UNIVERSITY PRESS, 1992,
 // ISBN:978-0-521-43108-8
 
@@ -13,28 +13,28 @@ class CubicSplineInterp {
 private:
     volatile bool _notValid = true;
     volatile bool _inCalc = false;
-	int _n; // number of tabulated points
-	float _y2[CSI_MAX_TAB_POINTS]; // contains the second derivatives of the interpolating function at the tabulated points 
-	const float* _x;
-	const float* _y;
-    
+    int _n; // number of tabulated points
+    float _y2[CSI_MAX_TAB_POINTS]; // contains the second derivatives of the interpolating function at the tabulated points
+    const float* _x;
+    const float* _y;
+
     int _klo;
     void _hunt(float x, int *jlo);
-	
+
 public:
-	/**
-	 *  \brief Brief
-	 *  
-	 *  \param [in] x[]
-	 *  \param [in] y[]
-	 *  \param [in] n Length of x[] and y[]
-	 *  \return 
-	 *  
-	 *  \details Details
-	 */
-	void init(const float* x, const float* y, int n);
-	
-	float calc(float x) const;
+    /**
+     *  \brief Brief
+     *
+     *  \param [in] x[]
+     *  \param [in] y[]
+     *  \param [in] n Length of x[] and y[]
+     *  \return
+     *
+     *  \details Details
+     */
+    void init(const float* x, const float* y, int n);
+
+    float calc(float x) const;
     float calcHunt(float x);
 };
 
